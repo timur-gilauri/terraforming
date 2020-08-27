f3ca3271092740458c16282dfdf194b3d9b8847b
timur_gilauri <timur_gilauri@epam.com>
2020-08-27 22:23:08 +0300

    init

---
+++ init.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+	profile = "default"
+	region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "teams-dynamodb-table" {
+	name			= "Teams"
+	read_capacity	= 20
+	write_capacity	= 20
+	hash_key		= "TeamTitle"
+	range_key		= "PlayerName"
+
+	attribute {
+		name = "TeamTitle"
+		type = "S" 
+	}
+
+	attribute {
+		name = "PlayerName"
+		type = "S" 
+	}
+}
+
+locals {
+	items_vars = {
+		table_name		= "${aws_dynamodb_table.teams-dynamodb-table.name}"
+		hash_key		= "${aws_dynamodb_table.teams-dynamodb-table.hash_key}"
+	}
+	players_path = "${path.module}/players.json"
+}
+
+resource "aws_dynamodb_table_item" "the-players" {
+	for_each = toset(jsondecode(file(local.players_path)))
+
+	table_name = local.items_vars.table_name
+	hash_key = local.items_vars.hash_key
+
+	item = each.value	
+}
