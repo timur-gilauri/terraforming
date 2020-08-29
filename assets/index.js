388983471e25ad1bc9475290241e225b3e9c1ef8
timur_gilauri <timur_gilauri@epam.com>
2020-08-29 17:52:01 +0300

    get items from db

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,15 @@
-exports.handler = (event, context, callback) => {
-    console.log('Hello, logs!');
-    callback(null, 'great success');
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context, callback) => {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+    try {
+        const result = await dynamodb.scan({
+            TableName: "Teams"
+        }).promise();
+
+        return context.succeed(result.Items);
+    } catch (e) {
+        return context.succeed(e);
+    }
 }
