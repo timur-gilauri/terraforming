d38f9575c78432d60f380aa0c88544da35a6dc21
timur_gilauri <timur_gilauri@epam.com>
2020-08-31 12:11:26 +0300

    explicit path to players items

--- modules/dynamodb/dynamodb.tf
+++ modules/dynamodb/dynamodb.tf
@@ -1,36 +1,31 @@
-variable "source_path" {
-    type = string
-}
-
 locals {
-    players = jsondecode(file(var.source_path))
+  players = jsondecode(file("${path.module}/players.json"))
 }
-
 
 resource "aws_dynamodb_table" "teams-dynamodb-table" {
-    name           = "Teams"
-    read_capacity  = 20
-    write_capacity = 20
-    hash_key       = "TeamTitle"
-    range_key      = "PlayerName"
+  name           = "Teams"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "TeamTitle"
+  range_key      = "PlayerName"
 
-    attribute {
-        name = "TeamTitle"
-        type = "S"
-    }
+  attribute {
+    name = "TeamTitle"
+    type = "S"
+  }
 
-    attribute {
-        name = "PlayerName"
-        type = "S"
-    }
+  attribute {
+    name = "PlayerName"
+    type = "S"
+  }
 }
 
 resource "aws_dynamodb_table_item" "the-players" {
-    count = length(local.players)
+  count = length(local.players)
 
-    table_name = aws_dynamodb_table.teams-dynamodb-table.name
-    hash_key   = aws_dynamodb_table.teams-dynamodb-table.hash_key
-    range_key  = aws_dynamodb_table.teams-dynamodb-table.range_key
+  table_name = aws_dynamodb_table.teams-dynamodb-table.name
+  hash_key   = aws_dynamodb_table.teams-dynamodb-table.hash_key
+  range_key  = aws_dynamodb_table.teams-dynamodb-table.range_key
 
-    item = jsonencode(local.players[ count.index ])
+  item = jsonencode(local.players[count.index])
 }
