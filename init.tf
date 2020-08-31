3ba1ab802295bafddf054378989f07eaf1f89a95
timur_gilauri <timur_gilauri@epam.com>
2020-08-31 12:12:38 +0300

    remove bucket module from base config

--- init.tf
+++ init.tf
@@ -1,55 +1,45 @@
 provider "aws" {
-    profile = "default"
-    region  = "us-east-1"
-}
-
-module "bucket" {
-    source = "./modules/bucket"
-
-    handler_path = "${path.module}/assets/index.js"
-    output_path  = "${path.module}/assets/tmp/lambda.zip"
+  profile = "default"
+  region  = "us-east-1"
 }
 
 module "dynamodb" {
-    source = "./modules/dynamodb"
-
-    source_path = "${path.module}/assets/players.json"
+  source = "./modules/dynamodb"
 }
 
 module "api" {
-    source = "./modules/api"
+  source = "./modules/api"
 }
 
 module "lambda" {
-    source = "./modules/lambda"
+  source = "./modules/lambda"
 
-    bucket_name  = module.bucket.name
-    handler_key  = module.bucket.key
-    api_exec_arn = module.api.exec_arn
+  bucket_data  = jsondecode(file("./bucket_data.json"))
+  api_exec_arn = module.api.exec_arn
 
-    depends_on = [
-        module.bucket ]
+  depends_on = [
+  module.api]
 }
 
 module "integration" {
-    source = "./modules/integration"
+  source = "./modules/integration"
 
-    api_data          = module.api.data
-    lambda_invoke_arn = module.lambda.invoke_arn
-    lambda_name       = module.lambda.name
+  api_data          = module.api.data
+  lambda_invoke_arn = module.lambda.invoke_arn
+  lambda_name       = module.lambda.name
 
-    depends_on = [
-        module.api,
-        module.lambda ]
+  depends_on = [
+    module.api,
+  module.lambda]
 }
 
 module "deploy" {
-    source = "./modules/deploy"
+  source = "./modules/deploy"
 
-    api_id               = module.api.data.api_id
-    integration_name     = module.integration.name
-    invoke_url_save_path = "${path.module}/assets/tmp/invoke-url"
+  api_id               = module.api.data.api_id
+  integration_name     = module.integration.name
+  invoke_url_save_path = "${path.module}/assets/tmp/invoke-url"
 
-    depends_on = [
-        module.integration ]
+  depends_on = [
+  module.integration]
 }
