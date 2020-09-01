839e4d20731487c021bc971f578327896a61a94c
timur_gilauri <timur_gilauri@epam.com>
2020-09-01 13:26:54 +0300

    create api for two handlers

--- modules/api/api.tf
+++ modules/api/api.tf
@@ -1,33 +1,55 @@
 resource "aws_api_gateway_rest_api" "api" {
-    name = "test-api"
+  name = "test-api"
 
-    endpoint_configuration {
-        types = [
-            "REGIONAL" ]
-    }
+  endpoint_configuration {
+    types = [
+    "REGIONAL"]
+  }
 }
 
-resource "aws_api_gateway_resource" "resource" {
-    rest_api_id = aws_api_gateway_rest_api.api.id
-    parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-    path_part   = "list"
+resource "aws_api_gateway_resource" "get" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "list"
 }
 
-resource "aws_api_gateway_method" "method" {
-    rest_api_id   = aws_api_gateway_rest_api.api.id
-    resource_id   = aws_api_gateway_resource.resource.id
-    http_method   = "GET"
-    authorization = "NONE"
+resource "aws_api_gateway_method" "method-get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.get.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_resource" "post" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "items"
+}
+
+resource "aws_api_gateway_method" "method-post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.post.id
+  http_method   = "POST"
+  authorization = "NONE"
 }
 
 output "data" {
-    value = {
-        api_id      = aws_api_gateway_rest_api.api.id
-        resource_id = aws_api_gateway_resource.resource.id
-        http_method = aws_api_gateway_method.method.http_method
+  value = {
+    api_id = aws_api_gateway_rest_api.api.id
+    api = {
+      get = {
+        resource_id = aws_api_gateway_resource.get.id
+        http_method = aws_api_gateway_method.method-get.http_method
+      }
+      post = {
+        resource_id = aws_api_gateway_resource.post.id
+        http_method = aws_api_gateway_method.method-post.http_method
+      }
     }
+
+  }
 }
 
 output "exec_arn" {
-    value = aws_api_gateway_rest_api.api.execution_arn
+  value = aws_api_gateway_rest_api.api.execution_arn
 }
